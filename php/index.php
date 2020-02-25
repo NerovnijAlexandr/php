eb61f10d76ab51203941c034a2984b8ca5dae492
krom9 <slonikzver1989@gmail.com>
2020-02-25 17:49:06 +0200

    then

--- php/index.php
+++ php/index.php
@@ -8,6 +8,18 @@ echo 2020;
 $name = 'Виктор';
 $name_company = 'IT2.0';
 
+$surname = 'Иванов';
+$name = 'Василий';
+$prefix = 'sub-';
+
+echo 'hi, ', $surname, $name;
+
+print "hi, $surname $name";
+
+echo "<h2>привет {$prefix}kk</h2>";
+
+define('ADMIN_NAME', 'test');
+echo ADMIN_NAME;
 
 // phpinfo();
 
