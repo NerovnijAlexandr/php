780565bfabdbcdfe01b8cf55f62a29fd36f2c632
krom9 <slonikzver1989@gmail.com>
2020-03-07 11:04:30 +0200

    less 6

---
+++ php/pdo/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    use Academy\Database;
+
+    require_once __DIR__ . '/vendor/autoload.php';
+
+    $db = new Database();
+
+    var_dump($db);
+
+?>
---
+++ php/pdo/src/Database.php
@@ -0,0 +1,10 @@
+<?php
+
+    namespace Academy;
+
+    class Database extends Connect
+    {
+
+    }
+
+?>
