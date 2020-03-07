7987941688e4f385223be1a6a313b413cc7962f9
krom9 <slonikzver1989@gmail.com>
2020-03-07 09:58:19 +0200

    less 6

---
+++ php/index_less_5.php
@@ -0,0 +1,38 @@
+<?php
+/*
+	abstract class Figure
+	{
+		protected $color = '';
+		protected $type = '';
+
+		public function setType($type)
+		{
+			$this->type = $type;
+		}
+
+		public function setColor($color)
+		{
+			$this->color = $color;
+		}
+
+		abstract public function calcSquare();
+	}	
+
+
+	class Circle extends Figure
+	{
+		public function calcSquare() 
+		{
+			return 'circle square';
+		}
+	}
+
+	// $figure = new Figure();
+	// echo $figure->calcSquare();
+	$circle = new Circle();
+	echo $circle->calcSquare();
+*/
+
+	
+
+?>
